57052e05348028c7e5eba46dd7e6bd89384e8f21
kker <907765631@qq.com>
2020-09-02 19:03:08 +0800

    [Mamingjun] feat: complete project

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class GrammarExercise {
     public static void main(String[] args) {
@@ -13,6 +16,33 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        if (hasTwoComma(firstWordList) || hasTwoComma(secondWordList) || containsInvalidSymbol(firstWordList)
+                || containsInvalidSymbol(secondWordList)) {
+            throw new RuntimeException();
+        }
+
+        return Stream.of(firstWordList.toUpperCase().split(","))
+                .distinct()
+                .filter(str -> Arrays.asList(secondWordList.toUpperCase().split(",")).contains(str))
+                .map(str -> str.split(""))
+                .map(chars -> String.join(" ", chars))
+                .collect(Collectors.toList());
+    }
+
+    public static boolean hasTwoComma(String word) {
+        return word.contains(",,");
+    }
+
+    public static boolean containsInvalidSymbol(String word) {
+        boolean res = false;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (Character.isLetter(ch) || ch ==',') {
+                continue;
+            }
+            res = true;
+            break;
+        }
+        return res;
     }
 }
